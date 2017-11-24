544024e514a0ab2b34ee7c489ea8abf028b6c035
amey <pethe.amey@yahoo.com>
2017-11-24 14:35:42 +0530

    module 1 folder

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -9,19 +9,19 @@
         $scope.lunchArray = [];
         $scope.lunchCount = 0;
         $scope.msgLunch = "";
-        $scope.lunchString = "";
         
         $scope.checkLunch = function () {
-            $scope.lunchArray = $scope.lunchString.split(",");
-            $scope.lunchCount = $scope.lunchArray.length;
-            if ($scope.lunchCount > 3) {
-                $scope.msgLunch = "Too much!";
-            } else {
-                $scope.msgLunch = "Enjoy!";
-            }
-            
             if ($scope.lunchString == "") {
                 $scope.msgLunch = "Please enter data first";                
+            } else {
+                $scope.lunchArray = $scope.lunchString.split(",");
+                $scope.lunchCount = $scope.lunchArray.length;
+            
+                if ($scope.lunchCount > 3) {
+                    $scope.msgLunch = "Too much!";
+                } else {
+                    $scope.msgLunch = "Enjoy!";
+                }
             }
         };
     }

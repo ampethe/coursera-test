5f4064cd1816d567756c50fd95a50bfe366b2ce2
Amey <pethe.amey@yahoo.com>
2017-12-09 21:48:10 +0530

    module 3 assignment

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,111 +1,39 @@
 (function () {
-'use strict';
-
-angular.module('ShoppingListDirectiveApp', [])
-.controller('ShoppingListController', ShoppingListController)
-.factory('ShoppingListFactory', ShoppingListFactory)
-.directive('shoppingList', ShoppingListDirective);
-
-
-function ShoppingListDirective() {
-  var ddo = {
-    templateUrl: 'shoppingList.html',
-    scope: {
-      items: '<',
-      myTitle: '@title',
-      badRemove: '=',
-      onRemove: '&'
-    },
-    controller: ShoppingListDirectiveController,
-    controllerAs: 'list',
-    bindToController: true
-  };
-
-  return ddo;
-}
-
-
-function ShoppingListDirectiveController() {
-  var list = this;
-
-  list.cookiesInList = function () {
-    for (var i = 0; i < list.items.length; i++) {
-      var name = list.items[i].name;
-      if (name.toLowerCase().indexOf("cookie") !== -1) {
-        return true;
-      }
+    'use strict';
+    
+    angular.module('NarrowItDownApp', [])
+    .controller('NarrowItDownController', NarrowItDownController)
+    .service('MenuSearchService', MenuSearchService)
+    .directive('MenuItemListDirective', MenuItemListDirective)
+    .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+    
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController (MenuSearchService) {
+        var narrow = this;
+        
+        narrow.narrowList = function () {
+            var narrowedItems = MenuSearchService.getMatchedMenuItems(narrow.itemName);    
+        }
     }
+    
+    MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+    function MenuSearchService ($http, ApiBasePath) {
+        var service = this;
+        
+        service.getMatchedMenuItems = function (searchTerm) {
+            var response = $http({
+              method: "GET",
+              url: (ApiBasePath + "/categories.json")
+            }).then(function (response) {
+                        //process result and only keep items that matched
+                        //var foundItems = 
+                        console.log(response.data);
+                        
+                        //return processed items
+                        return foundItems;
+                    });
 
-    return false;
-  };
-}
-
-
-ShoppingListController.$inject = ['ShoppingListFactory'];
-function ShoppingListController(ShoppingListFactory) {
-  var list = this;
-
-  // Use factory to create new shopping list service
-  var shoppingList = ShoppingListFactory();
-
-  list.items = shoppingList.getItems();
-  var origTitle = "Shopping List #1";
-  list.title = origTitle + " (" + list.items.length + " items )";
-
-  list.itemName = "";
-  list.itemQuantity = "";
-
-  list.addItem = function () {
-    shoppingList.addItem(list.itemName, list.itemQuantity);
-    list.title = origTitle + " (" + list.items.length + " items )";
-  };
-
-  list.removeItem = function (itemIndex) {
-    console.log("'this' is: ", this);
-    this.lastRemoved = "Last item removed was " + this.items[itemIndex].name;
-    shoppingList.removeItem(itemIndex);
-    this.title = origTitle + " (" + list.items.length + " items )";
-  };
-}
-
-
-// If not specified, maxItems assumed unlimited
-function ShoppingListService(maxItems) {
-  var service = this;
-
-  // List of shopping items
-  var items = [];
-
-  service.addItem = function (itemName, quantity) {
-    if ((maxItems === undefined) ||
-        (maxItems !== undefined) && (items.length < maxItems)) {
-      var item = {
-        name: itemName,
-        quantity: quantity
-      };
-      items.push(item);
+            return response;
+        };
     }
-    else {
-      throw new Error("Max items (" + maxItems + ") reached.");
-    }
-  };
-
-  service.removeItem = function (itemIndex) {
-    items.splice(itemIndex, 1);
-  };
-
-  service.getItems = function () {
-    return items;
-  };
-}
-
-
-function ShoppingListFactory() {
-  var factory = function (maxItems) {
-    return new ShoppingListService(maxItems);
-  };
-
-  return factory;
-}
-
 })();
